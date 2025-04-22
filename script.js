2b1d1f34abc73c330e99f98cb75ddc828f06e1de
CL <leungc22@gmail.com>
2025-04-22 11:30:12 -0400

    part 2

---
+++ script.js
@@ -0,0 +1,27 @@
+class ProductProperties {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    getTotalValue() {
+        return this.price * this.quantity;
+    }
+
+    toString() {
+        return `Product: ${this.name} Price: ${this.price} Quantity: ${this.quantity}`;
+    }
+}
+
+    class PerishableProductProperties extends ProductProperties {
+        constructor(name, price, quantity, expirationDate) {
+            super(name, price, quantity);
+            this.expirationDate = expirationDate;
+        }
+    
+        toString() {
+            return `${super.toString()} Expiration Date: ${this.expirationDate}`;
+        }
+    }
+
+console.log("getTotalValue()");
